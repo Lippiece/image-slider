8c226fd9c60a8f4d3e90e187374411d4f3b65cb7
Lippiece <seriouss7222@gmail.com>
2022-08-21 12:15:24 +0200

    refactor to use classes

--- dist/main.js
+++ dist/main.js
@@ -2482,112 +2482,124 @@ const _body                 = document.querySelector( "body" )
 		color          : "var( --mainColor )",
 	} ) );
 
-initializeImageSlider( document.querySelector( "#content" ), [_1_jfif__WEBPACK_IMPORTED_MODULE_1__, _2_jfif__WEBPACK_IMPORTED_MODULE_2__, _3_jfif__WEBPACK_IMPORTED_MODULE_3__] );
-function initializeImageSlider( parent, images )
+class ImageSlider
 {
-	const container = document.createElement( "div" )
-			.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
-				display       : "flex",
-				flexDirection : "column",
-				justifyContent: "center",
-			} ) )
-			.addId( "image-slider-container" ),
-		imageSlider = document.createElement( "div" )
-			.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
-				maxWidth     : "100vw",
-				height       : "100%",
-				display      : "flex",
-				flexDirection: "row",
-				gap          : "0.5em",
-				overflow     : "hidden",
-			} ) )
-			.addId( "image-slider" )
-			.appendTo( container );
-
-	populateSlider( imageSlider, images );
-	addListeners( imageSlider );
-	addScrollProgress( imageSlider );
-	parent.append( container );
-}
-/**
-	* It takes a parent element and an array of image sources, and then creates an image element for each
-	* source, and appends it to the parent
-	* @param parent - The parent element to append the images to.
-	* @param images - An array of image sources.
-	*/
-function populateSlider( parent, images )
-{
-	for ( const source of images )
+	initializeImageSlider( parent, images )
 	{
-		const image = document.createElement( "img" );
+		const container = document.createElement( "div" )
+				.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
+					display       : "flex",
+					flexDirection : "column",
+					justifyContent: "center",
+				} ) )
+				.addId( "image-slider-container" ),
+			imageSlider = document.createElement( "div" )
+				.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
+					maxWidth     : "100vw",
+					height       : "100%",
+					display      : "flex",
+					flexDirection: "row",
+					gap          : "0.5em",
+					overflow     : "hidden",
+				} ) )
+				.addId( "image-slider" )
+				.appendTo( container );
 
-		image
+		this.div = imageSlider;
+		this.populateSlider( images );
+		this.addListeners( imageSlider );
+		   this.addScrollProgress( imageSlider );
+		parent.append( container );
+	}
+	populateSlider( images )
+	{
+		for ( const source of images )
+		{
+			new Image( source, this )
+				.addId( `image-${ images.indexOf( source )+1 }` );
+		}
+	}
+	addListeners( imageSlider )
+	{
+		/* Scroll horizontally */
+		imageSlider.addEventListener( "wheel", ( event ) =>
+		{
+			event.preventDefault();
+			imageSlider.scrollLeft += event.deltaY/2;
+		} );
+	}
+	addScrollProgress( imageSlider )
+	{
+		const scrollProgress = document.createElement( "div" )
+			.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
+				position       : "relative",
+				left           : "25%",
+				height         : "0.5em",
+				width          : "0%",
+				backgroundColor: "var( --mainColor )",
+				transition     : "all 0.5s ease",
+			} ) )
+			.addId( "scroll-progress" )
+			.appendTo( imageSlider.parentElement );
+
+		imageSlider.addEventListener( "scroll", () =>
+		{ scrollProgress.style.width = `${ imageSlider.scrollLeft / ( imageSlider.scrollWidth - imageSlider.clientWidth ) * 50 }%` } );
+	}
+	constructor( parent, images )
+	{ this.initializeImageSlider( parent, images ) }
+}
+class Image
+{
+	toggleCurrentImage( image )
+	{
+		image.classList.toggle( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
+			marginLeft : "2em",
+			marginRight: "2em",
+			height     : "30em",
+		} ) );
+	}
+	removeCurrentIfPresent( image )
+	{
+		if ( image.classList.contains( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
+			marginLeft : "2em",
+			marginRight: "2em",
+			height     : "30em",
+		} ) ) ) { this.toggleCurrentImage( image ) }
+	}
+	resetCurrentImage()
+	{
+		for ( const image of this.slider.div.children )
+		{ this.removeCurrentIfPresent( image ) }
+	}
+	addEventListeners( )
+	{
+		this.img.addEventListener( "click", ( event ) =>
+		{
+			this.resetCurrentImage( this.slider );
+			this.toggleCurrentImage( event.target );
+			this.slider.div.scrollLeft = event.target.offsetLeft - this.slider.div.offsetLeft;
+		} );
+
+	}
+	constructor( source, slider )
+	{
+		this.img = document.createElement( "img" );
+		this.img
 			.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
 				height    : "20em",
 				cursor    : "pointer",
 				transition: "all 0.5s ease",
-			 } ) )
-			.addId( `image-${ images.indexOf( source )+1 }` )
+				 } ) )
 			.src = source;
-		parent.append( image );
+		slider.div.append( this.img );
+		this.slider = slider;
+		this.source = source;
+		this.addEventListeners();
+
+		return this.img;
 	}
 }
-function toggleCurrentImage( image )
-{
-	image.classList.toggle( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
-		marginLeft : "2em",
-		marginRight: "2em",
-		height     : "30em",
-	} ) );
-}
-function addListeners( imageSlider )
-{
-	imageSlider.addEventListener( "click", ( event ) =>
-	{
-		if ( event.target.tagName === "IMG" )
-		{
-			resetCurrentImage( imageSlider );
-			toggleCurrentImage( event.target );
-			imageSlider.scrollLeft = event.target.offsetLeft - imageSlider.offsetLeft;
-		}
-	} );
-	/* Scroll horizontally */
-	imageSlider.addEventListener( "wheel", ( event ) =>
-	{
-		event.preventDefault();
-		imageSlider.scrollLeft += event.deltaY/2;
-	} );
-}
-function removeCurrentIfPresent( image )
-{
-	if ( image.classList.contains( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
-		marginLeft : "2em",
-		marginRight: "2em",
-		height     : "30em",
-	} ) ) ) { toggleCurrentImage( image ) }
-}
-function resetCurrentImage( imageSlider )
-{
-	for ( const image of imageSlider.children )
-	{ removeCurrentIfPresent( image ) }
-}
-function addScrollProgress( imageSlider )
-{
-	const scrollProgress = document.createElement( "div" )
-		.addStyles( (0,_emotion_css__WEBPACK_IMPORTED_MODULE_0__.css)( {
-			position       : "relative",
-			left           : "25%",
-			height         : "0.5em",
-			width          : "0%",
-			backgroundColor: "var( --mainColor )",
-			transition     : "all 0.5s ease",
-		} ) )
-		.addId( "scroll-progress" )
-		.appendTo( imageSlider.parentElement );
-
-	imageSlider.addEventListener( "scroll", () =>
-	{ scrollProgress.style.width = `${ imageSlider.scrollLeft / ( imageSlider.scrollWidth - imageSlider.clientWidth ) * 50 }%` } );
-}
+const _testSlider = new ImageSlider( document.querySelector( "#content" ), [_1_jfif__WEBPACK_IMPORTED_MODULE_1__, _2_jfif__WEBPACK_IMPORTED_MODULE_2__, _3_jfif__WEBPACK_IMPORTED_MODULE_3__] );
 
 })();
 
--- src/main/main.js
+++ src/main/main.js
@@ -35,109 +35,121 @@ const _body                 = document.querySelector( "body" )
 		color          : "var( --mainColor )",
 	} ) );
 
-initializeImageSlider( document.querySelector( "#content" ), [source1, source2, source3] );
-function initializeImageSlider( parent, images )
+class ImageSlider
 {
-	const container = document.createElement( "div" )
-			.addStyles( css( {
-				display       : "flex",
-				flexDirection : "column",
-				justifyContent: "center",
-			} ) )
-			.addId( "image-slider-container" ),
-		imageSlider = document.createElement( "div" )
-			.addStyles( css( {
-				maxWidth     : "100vw",
-				height       : "100%",
-				display      : "flex",
-				flexDirection: "row",
-				gap          : "0.5em",
-				overflow     : "hidden",
-			} ) )
-			.addId( "image-slider" )
-			.appendTo( container );
-
-	populateSlider( imageSlider, images );
-	addListeners( imageSlider );
-	addScrollProgress( imageSlider );
-	parent.append( container );
-}
-/**
-	* It takes a parent element and an array of image sources, and then creates an image element for each
-	* source, and appends it to the parent
-	* @param parent - The parent element to append the images to.
-	* @param images - An array of image sources.
-	*/
-function populateSlider( parent, images )
-{
-	for ( const source of images )
+	initializeImageSlider( parent, images )
 	{
-		const image = document.createElement( "img" );
+		const container = document.createElement( "div" )
+				.addStyles( css( {
+					display       : "flex",
+					flexDirection : "column",
+					justifyContent: "center",
+				} ) )
+				.addId( "image-slider-container" ),
+			imageSlider = document.createElement( "div" )
+				.addStyles( css( {
+					maxWidth     : "100vw",
+					height       : "100%",
+					display      : "flex",
+					flexDirection: "row",
+					gap          : "0.5em",
+					overflow     : "hidden",
+				} ) )
+				.addId( "image-slider" )
+				.appendTo( container );
 
-		image
+		this.div = imageSlider;
+		this.populateSlider( images );
+		this.addListeners( imageSlider );
+		   this.addScrollProgress( imageSlider );
+		parent.append( container );
+	}
+	populateSlider( images )
+	{
+		for ( const source of images )
+		{
+			new Image( source, this )
+				.addId( `image-${ images.indexOf( source )+1 }` );
+		}
+	}
+	addListeners( imageSlider )
+	{
+		/* Scroll horizontally */
+		imageSlider.addEventListener( "wheel", ( event ) =>
+		{
+			event.preventDefault();
+			imageSlider.scrollLeft += event.deltaY/2;
+		} );
+	}
+	addScrollProgress( imageSlider )
+	{
+		const scrollProgress = document.createElement( "div" )
+			.addStyles( css( {
+				position       : "relative",
+				left           : "25%",
+				height         : "0.5em",
+				width          : "0%",
+				backgroundColor: "var( --mainColor )",
+				transition     : "all 0.5s ease",
+			} ) )
+			.addId( "scroll-progress" )
+			.appendTo( imageSlider.parentElement );
+
+		imageSlider.addEventListener( "scroll", () =>
+		{ scrollProgress.style.width = `${ imageSlider.scrollLeft / ( imageSlider.scrollWidth - imageSlider.clientWidth ) * 50 }%` } );
+	}
+	constructor( parent, images )
+	{ this.initializeImageSlider( parent, images ) }
+}
+class Image
+{
+	toggleCurrentImage( image )
+	{
+		image.classList.toggle( css( {
+			marginLeft : "2em",
+			marginRight: "2em",
+			height     : "30em",
+		} ) );
+	}
+	removeCurrentIfPresent( image )
+	{
+		if ( image.classList.contains( css( {
+			marginLeft : "2em",
+			marginRight: "2em",
+			height     : "30em",
+		} ) ) ) { this.toggleCurrentImage( image ) }
+	}
+	resetCurrentImage()
+	{
+		for ( const image of this.slider.div.children )
+		{ this.removeCurrentIfPresent( image ) }
+	}
+	addEventListeners( )
+	{
+		this.img.addEventListener( "click", ( event ) =>
+		{
+			this.resetCurrentImage( this.slider );
+			this.toggleCurrentImage( event.target );
+			this.slider.div.scrollLeft = event.target.offsetLeft - this.slider.div.offsetLeft;
+		} );
+
+	}
+	constructor( source, slider )
+	{
+		this.img = document.createElement( "img" );
+		this.img
 			.addStyles( css( {
 				height    : "20em",
 				cursor    : "pointer",
 				transition: "all 0.5s ease",
-			 } ) )
-			.addId( `image-${ images.indexOf( source )+1 }` )
+				 } ) )
 			.src = source;
-		parent.append( image );
+		slider.div.append( this.img );
+		this.slider = slider;
+		this.source = source;
+		this.addEventListeners();
+
+		return this.img;
 	}
 }
-function toggleCurrentImage( image )
-{
-	image.classList.toggle( css( {
-		marginLeft : "2em",
-		marginRight: "2em",
-		height     : "30em",
-	} ) );
-}
-function addListeners( imageSlider )
-{
-	imageSlider.addEventListener( "click", ( event ) =>
-	{
-		if ( event.target.tagName === "IMG" )
-		{
-			resetCurrentImage( imageSlider );
-			toggleCurrentImage( event.target );
-			imageSlider.scrollLeft = event.target.offsetLeft - imageSlider.offsetLeft;
-		}
-	} );
-	/* Scroll horizontally */
-	imageSlider.addEventListener( "wheel", ( event ) =>
-	{
-		event.preventDefault();
-		imageSlider.scrollLeft += event.deltaY/2;
-	} );
-}
-function removeCurrentIfPresent( image )
-{
-	if ( image.classList.contains( css( {
-		marginLeft : "2em",
-		marginRight: "2em",
-		height     : "30em",
-	} ) ) ) { toggleCurrentImage( image ) }
-}
-function resetCurrentImage( imageSlider )
-{
-	for ( const image of imageSlider.children )
-	{ removeCurrentIfPresent( image ) }
-}
-function addScrollProgress( imageSlider )
-{
-	const scrollProgress = document.createElement( "div" )
-		.addStyles( css( {
-			position       : "relative",
-			left           : "25%",
-			height         : "0.5em",
-			width          : "0%",
-			backgroundColor: "var( --mainColor )",
-			transition     : "all 0.5s ease",
-		} ) )
-		.addId( "scroll-progress" )
-		.appendTo( imageSlider.parentElement );
-
-	imageSlider.addEventListener( "scroll", () =>
-	{ scrollProgress.style.width = `${ imageSlider.scrollLeft / ( imageSlider.scrollWidth - imageSlider.clientWidth ) * 50 }%` } );
-}
+const _testSlider = new ImageSlider( document.querySelector( "#content" ), [source1, source2, source3] );
